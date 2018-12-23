99b36cf6f077723bcb543c3c85ef146bf2e6363c
dkorwar <42906055+deeptikorwar@users.noreply.github.com>
2018-12-23 21:31:33 +1100

    Update FuncScript.py

--- SampFunc/FuncScript.py
+++ SampFunc/FuncScript.py
@@ -4,7 +4,7 @@
 import FuncFile
 import math
 
-#To experiment, change the number in the list
+#To experiment, change the numbers in the list
 mynum=[4,5,10]
 
 mynumsum=FuncFile.fn_add(mynum)
@@ -13,4 +13,4 @@ print("The sum of the numbers is {}".format(mynumsum))
 
 num=5
 
-print("The exponential value of 5 is {}".format(math.exp(num)))
+print("The exponential value of {} is {}".format(num,math.exp(num)))
