7c4ba88a702a0a3331a694a9edd2224df71c42bb
dkorwar <42906055+deeptikorwar@users.noreply.github.com>
2018-12-31 22:11:54 +0530

    Update myturtle.py

--- myTurtles/myturtle.py
+++ myTurtles/myturtle.py
@@ -1,8 +1,10 @@
+"""This script shows you how to use the turtle module to draw squares."""
 import turtle
 
 window = turtle.Screen() #the canvas on which the turtle can draw
 window.bgcolor("red")
 
+#create an instance of Turtle class and draw an yellow square
 brad = turtle.Turtle()
 brad.color("yellow")
 brad.shape("turtle")
@@ -14,6 +16,7 @@ brad.forward(100)
 brad.right(90)
 brad.forward(100)
 
+#create an instance of Turtle class and draw a blue square
 geoff = turtle.Turtle()
 geoff.color("blue")
 geoff.shape("turtle")
